c7aff919f62054c850f417885ec897320308910a
Edfram <edfram.guerra@gmail.com>
2022-09-01 13:31:34 -0300

    resolução do exercico 06

--- 06/index.js
+++ 06/index.js
@@ -3,3 +3,5 @@ const tempo = 10; // segundos
 const velocidade = (distanciaPercorrida / tempo) * 3.6; // km/h
 
 console.log(`A velocidade média da distância percorrida foi de ${velocidade} km/h`);
+
+// enunciado errado - acho que ao invés de 500 é 50 , fiz com 50 m
