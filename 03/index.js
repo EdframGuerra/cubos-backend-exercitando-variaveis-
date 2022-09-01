12f71b35851a8a6efce5b091147acfe361b65a64
Edfram <edfram.guerra@gmail.com>
2022-08-31 23:22:26 -0300

    resolução do exercício 03

---
+++ 03/index.js
@@ -0,0 +1,6 @@
+let valorTenis = 129.99;
+let meuDinheiro = 80.00;
+let valorDesconto = valorTenis - meuDinheiro;
+let percentualDesconto = valorDesconto * 100 / valorTenis
+
+console.log(percentualDesconto);
