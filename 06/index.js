9270366f388724c33dbd3d5eb9d3fb68677ee897
Edfram <edfram.guerra@gmail.com>
2022-09-01 19:28:26 -0300

    Resolução do exercico 11

--- 06/index.js
+++ 06/index.js
@@ -1,7 +1,5 @@
-const distanciaPercorrida = 50; //metros
-const tempo = 10; // segundos
-const velocidade = (distanciaPercorrida / tempo) * 3.6; // km/h
+const distanciaPercorrida = 500;
+const tempo = 10;
+const velocidade = (distanciaPercorrida / tempo) * 3.6;
 
 console.log(`A velocidade média da distância percorrida foi de ${velocidade} km/h`);
-
-// enunciado errado - acho que ao invés de 500 é 50 , fiz com 50 m
