8170208eb24b719393818938e9974d288d4fbfee
Edfram <edfram.guerra@gmail.com>
2022-09-01 19:39:21 -0300

    resolução do exercico 08

---
+++ 08/index.js
@@ -0,0 +1,12 @@
+//Qual o cumprimento do circulo? 2PiR
+// Qual a area do circulo? PiR**2
+
+const pi = 3.14159;
+const raio = 1;
+const comprimentoDoCirculo = 2 * pi * raio
+const areaDoCirculo = pi * raio ** 2
+
+
+console.log(comprimentoDoCirculo);
+console.log(areaDoCirculo);
+
