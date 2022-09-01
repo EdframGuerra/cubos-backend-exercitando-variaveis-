2d86b8f49c67e9622676a026ff37c6a1a5dc2927
Edfram <edfram.guerra@gmail.com>
2022-09-01 13:23:08 -0300

    Resolução do exercício 09

--- 09/index.js
+++ 09/index.js
@@ -1,7 +1,3 @@
-// valor de angulo (a)  = soma dos angulos (S)/ numero de lados (n)
-// soma de angulos (S) = (numero de lados (n) - 2) * 180
-// poligono de 06 lados com angulos de 120° garus
-
 const numeroDelados = 6;
 const somaDeAngulos = (numeroDelados - 2) * 180;
 const valorDoAngulo = somaDeAngulos / numeroDelados;
