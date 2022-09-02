ab3b88351fc104f5ee33bb5e15e8a54227303d21
Edfram <edfram.guerra@gmail.com>
2022-09-02 20:38:23 -0300

    exercicio resolvido

--- 07/index.js
+++ 07/index.js
@@ -1,5 +1,5 @@
-const populacaoInfectadaInicialmente = 1_000; // Po
-const qtdDeInfectadosPorUmPaciente = 4; // x
+const populacaoInfectadaInicialmente = 1_000;
+const qtdDeInfectadosPorUmPaciente = 4;
 const tempo = 100;
 const pessoasInfectadas = populacaoInfectadaInicialmente * qtdDeInfectadosPorUmPaciente ** (100 / 100);
 
