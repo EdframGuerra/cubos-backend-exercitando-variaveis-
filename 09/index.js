e2cfaee053e249907198335f2f543ec692cb6599
Edfram <edfram.guerra@gmail.com>
2022-09-02 20:41:43 -0300

    Exercicio resolvido

--- 09/index.js
+++ 09/index.js
@@ -2,4 +2,4 @@ const numeroDelados = 6;
 const somaDeAngulos = (numeroDelados - 2) * 180;
 const valorDoAngulo = somaDeAngulos / numeroDelados;
 
-console.log(`O valor da soma de angulos é ${somaDeAngulos} graus e o valor de cada angulo é ${valorDoAngulo}`);
+console.log(`O valor da soma de angulos é ${somaDeAngulos} graus e o valor de cada angulo é ${valorDoAngulo} graus`);
