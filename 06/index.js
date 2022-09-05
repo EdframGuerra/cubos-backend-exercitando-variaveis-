dcddeec910c0082fa511d27be428eb0182ed3866
Edfram <edfram.guerra@gmail.com>
2022-09-05 11:18:39 -0300

    Exercício resolvido e revisado!

--- 06/index.js
+++ 06/index.js
@@ -1,5 +1,5 @@
 const distanciaPercorrida = 500;
 const tempo = 10;
-const velocidade = (distanciaPercorrida / tempo) * 3.6;
+const velocidade = distanciaPercorrida / tempo * 3.6;
 
 console.log(`A velocidade média da distância percorrida foi de ${velocidade} km/h`);
