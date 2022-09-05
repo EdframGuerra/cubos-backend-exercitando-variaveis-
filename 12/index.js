5f4321edcaf963cfefd7d029842513fc0493e9b9
Edfram <edfram.guerra@gmail.com>
2022-09-05 11:35:39 -0300

    Exercício resolvido e revisado!

--- 12/index.js
+++ 12/index.js
@@ -3,5 +3,7 @@ const capital = 60_000;
 const numeroDeMeses = 24;
 
 const taxaDeJuros = ((montante / capital) ** (1 / 24)) - 1;
+
 const jurosEmPercent = taxaDeJuros * 100;
+
 console.log(`O seu financiamento de ${capital} reais teve uma taxa de juros de ${jurosEmPercent} %, pois após ${numeroDeMeses} meses você teve que pagar ${montante} reais`);
