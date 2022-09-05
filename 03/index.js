cfcf61b57e7a54cc81bcac4cf73d1e16b68dff3a
Edfram <edfram.guerra@gmail.com>
2022-09-05 11:12:14 -0300

    Exercício resolvido e revisado!

--- 03/index.js
+++ 03/index.js
@@ -1,6 +1,8 @@
 let valorTenis = 129.99;
 let meuDinheiro = 80.00;
+
 let valorDesconto = valorTenis - meuDinheiro;
+
 let percentualDesconto = valorDesconto * 100 / valorTenis
 
 console.log(`Para conseguir comprar o tênis Eu precisdo de cupom ${percentualDesconto} %`);
