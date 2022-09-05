a6fd8c4eb853ef95c3396780277164b50bedd306
Edfram <edfram.guerra@gmail.com>
2022-09-05 11:25:00 -0300

    Exercício resolvido e revisado!

--- 08/index.js
+++ 08/index.js
@@ -3,7 +3,6 @@ const raio = 1;
 const comprimentoDoCirculo = 2 * pi * raio
 const areaDoCirculo = pi * raio ** 2
 
+console.log(`o Comprimento do circulo é ${comprimentoDoCirculo}`);
 
-console.log(comprimentoDoCirculo);
-console.log(areaDoCirculo);
-
+console.log(`a área do circulo é ${areaDoCirculo}`);
