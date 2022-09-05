57255ea8c91c87ef82d509051161007e6fe015c9
Edfram <edfram.guerra@gmail.com>
2022-09-05 11:16:15 -0300

    Exercício resolvido e revisado!

--- 05/index.js
+++ 05/index.js
@@ -2,6 +2,7 @@ const pontoX1 = 1;
 const pontoX2 = 1;
 const pontoY1 = 1;
 const pontoY2 = 4;
+
 const distancia = Math.sqrt((pontoX2 - pontoX1) ** 2 + (pontoY2 - pontoY1) ** 2);
 
 console.log(distancia);
